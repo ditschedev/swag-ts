e6f958de5ecfc490a2bdb0d48585ebc00e0ce350
Tobias Dittmann <hello@ditsche.dev>
2023-06-07 11:05:31 +0200

    improvement: test coverage

--- internal/generator/ts_generator_test.go
+++ internal/generator/ts_generator_test.go
@@ -36,3 +36,34 @@ export interface LoginResponseWrapper {
 		t.Errorf("Expected testtypes.ts to be:\n%s\n\nGot:\n%s", expected, string(data))
 	}
 }
+
+func TestNumberTypesGeneration(t *testing.T) {
+	gen := NewGenerator("../../testdata/test-numbers.json", TypeScript)
+
+	if gen == nil {
+		t.Fatalf("Expected generator to be created, got nil")
+	}
+
+	err := gen.GenerateTypes("../../tmp/testtypes.ts")
+	if err != nil {
+		t.Fatalf("Expected typescript types to be generated, got error: %s", err)
+	}
+
+	data, err := os.ReadFile("../../tmp/testtypes.ts")
+	if err != nil {
+		t.Fatalf("Expected testtypes.ts to be read, got error: %s", err)
+	}
+
+	expected := `
+export interface NumberTypes {
+  anyNum?: number;
+  doubleNum: number;
+  floatNum?: number;
+  int32Num?: number;
+  int64Num?: number;
+  intNum?: number;
+}`
+	if !strings.Contains(string(data), expected) {
+		t.Errorf("Expected testtypes.ts to be:\n%s\n\nGot:\n%s", expected, string(data))
+	}
+}
