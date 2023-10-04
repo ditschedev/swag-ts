5adbe78dddf1fd1ddc5aae7140336e861debeb0d
Tobias Dittmann <hello@ditsche.dev>
2023-10-04 11:10:10 +0200

    add formdata functionality

--- internal/generator/ts_generator.go
+++ internal/generator/ts_generator.go
@@ -12,6 +12,8 @@ import (
 	"time"
 )
 
+const FormDataSuffix = "FormData"
+
 func (g *generator) generateTypescriptTypes() error {
 	color.Set(color.FgHiBlack)
 	g.s.Suffix = " Generating types page"
@@ -79,97 +81,44 @@ func generateTypes(doc *openapi3.T, outputPath string) error {
 			continue
 		}
 
-		typeString := schema.Type
-
-		if typeString == "" {
-			typeString = "object"
-		}
-
-		model := Model{
-			Name:   name,
-			Type:   typeString,
-			IsEnum: false,
-			Fields: make([]Field, 0),
-		}
-
-		if schema.Enum != nil {
-			model.IsEnum = true
-			model.Values = schema.Enum
-			models = append(models, model)
-			continue
-		}
-
-		var requiredFields []string
-		if schema.Required != nil {
-			requiredFields = schema.Required
-		}
-
-		for fieldName, field := range ref.Value.Properties {
-			var f Field
-
-			switch field.Value.Type {
-			case "array":
-				f = Field{
-					Name:     fieldName,
-					Nullable: field.Value.Nullable,
-					Optional: true,
-				}
-
-				if field.Value.Items.Ref != "" {
-					f.Type = parseRefName(field.Value.Items.Ref) + "[]"
-				} else {
-					f.Type = swaggerTypeToTypescriptType[field.Value.Items.Value.Type] + "[]"
-				}
-			case "object":
-				fieldType := "any"
-
-				if field.Ref != "" {
-					fieldType = parseRefName(field.Ref)
-				}
-
-				f = Field{
-					Name:     fieldName,
-					Type:     fieldType,
-					Nullable: field.Value.Nullable,
-					Optional: true,
-				}
-			default:
-				fieldType := field.Value.Type
-
-				if fieldType == "" {
-					fieldType = "object"
-				}
-
-				f = Field{
-					Name:     fieldName,
-					Type:     swaggerTypeToTypescriptType[fieldType],
-					Nullable: field.Value.Nullable,
-					Optional: true,
-				}
-
-				if field.Ref != "" {
-					f.Type = parseRefName(field.Ref)
-				}
-
-			}
-
-			for _, requiredField := range requiredFields {
-				if requiredField == fieldName {
-					f.Optional = false
-					break
-				}
-			}
-
-			model.Fields = append(model.Fields, f)
-
-		}
-
-		// sort fields by name
-		sort.Slice(model.Fields, func(i, j int) bool {
-			return model.Fields[i].Name < model.Fields[j].Name
-		})
+		model := parseSchema(name, schema)
 
 		models = append(models, model)
+	}
+
+	// add form data from unmapped schemas from paths
+	for _, path := range doc.Paths {
+
+		if path.Post != nil {
+			info := path.Post.RequestBody
+
+			model, err := handleFormDataRequestBody(path.Post.OperationID+FormDataSuffix, info)
+
+			if err == nil {
+				models = append(models, model)
+			}
+		}
+
+		if path.Put != nil {
+			info := path.Put.RequestBody
+
+			model, err := handleFormDataRequestBody(path.Put.OperationID+FormDataSuffix, info)
+
+			if err == nil {
+				models = append(models, model)
+			}
+		}
+
+		if path.Patch != nil {
+			info := path.Patch.RequestBody
+
+			model, err := handleFormDataRequestBody(path.Patch.OperationID+FormDataSuffix, info)
+
+			if err == nil {
+				models = append(models, model)
+			}
+		}
+
 	}
 
 	sort.Slice(models, func(i, j int) bool {
@@ -205,4 +154,124 @@ func generateTypes(doc *openapi3.T, outputPath string) error {
 	}
 
 	return nil
+}
+
+func parseSchema(name string, schema *openapi3.Schema) Model {
+	typeString := schema.Type
+
+	if typeString == "" {
+		typeString = "object"
+	}
+
+	model := Model{
+		Name:   name,
+		Type:   typeString,
+		IsEnum: false,
+		Fields: make([]Field, 0),
+	}
+
+	if schema.Enum != nil {
+		model.IsEnum = true
+		model.Values = schema.Enum
+		return model
+	}
+
+	var requiredFields []string
+	if schema.Required != nil {
+		requiredFields = schema.Required
+	}
+
+	for fieldName, field := range schema.Properties {
+		var f Field
+
+		switch field.Value.Type {
+		case "array":
+			f = Field{
+				Name:     fieldName,
+				Nullable: field.Value.Nullable,
+				Optional: true,
+			}
+
+			if field.Value.Items.Ref != "" {
+				f.Type = parseRefName(field.Value.Items.Ref) + "[]"
+			} else {
+				f.Type = swaggerTypeToTypescriptType[field.Value.Items.Value.Type] + "[]"
+			}
+		case "object":
+			fieldType := "any"
+
+			if field.Ref != "" {
+				fieldType = parseRefName(field.Ref)
+			}
+
+			f = Field{
+				Name:     fieldName,
+				Type:     fieldType,
+				Nullable: field.Value.Nullable,
+				Optional: true,
+			}
+		default:
+			fieldType := field.Value.Type
+
+			if fieldType == "" {
+				fieldType = "object"
+			}
+
+			f = Field{
+				Name:     fieldName,
+				Type:     swaggerTypeToTypescriptType[fieldType],
+				Nullable: field.Value.Nullable,
+				Optional: true,
+			}
+
+			if field.Ref != "" {
+				f.Type = parseRefName(field.Ref)
+			}
+
+			if field.Value.Format == "binary" {
+				f.Type = "Blob"
+			}
+
+		}
+
+		for _, requiredField := range requiredFields {
+			if requiredField == fieldName {
+				f.Optional = false
+				break
+			}
+		}
+
+		model.Fields = append(model.Fields, f)
+	}
+
+	// sort fields by name
+	sort.Slice(model.Fields, func(i, j int) bool {
+		return model.Fields[i].Name < model.Fields[j].Name
+	})
+
+	return model
+}
+
+func handleFormDataRequestBody(name string, body *openapi3.RequestBodyRef) (Model, error) {
+	if body == nil || body.Value == nil || body.Ref != "" {
+		return Model{}, fmt.Errorf("no form data body found")
+	}
+
+	formDataBody, ok := body.Value.Content["multipart/form-data"]
+
+	if !ok {
+		return Model{}, fmt.Errorf("no form data body found")
+	}
+
+	if formDataBody != nil && formDataBody.Schema != nil {
+		schema := formDataBody.Schema
+
+		if schema.Ref != "" {
+			return Model{}, fmt.Errorf("no form data body found")
+		}
+
+		return parseSchema(name, schema.Value), nil
+	}
+
+	return Model{}, fmt.Errorf("no form data body found")
 }
